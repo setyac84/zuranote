8e6c39b96de169f61d8393b2664360fc81d26459
setya <setyaachsanul82@gmail.com>
2026-03-04 14:20:58 +0700

    update supabase

--- supabase/functions/reset-member-password/index.ts
+++ supabase/functions/reset-member-password/index.ts
@@ -11,9 +11,9 @@ Deno.serve(async (req) => {
   }
 
   try {
-    const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
-    const serviceRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
-    const anonKey = Deno.env.get("SUPABASE_ANON_KEY")!;
+    const supabaseUrl = Deno.env.get("MY_SUPABASE_URL")!;
+    const serviceRoleKey = Deno.env.get("MY_SUPABASE_SERVICE_ROLE_KEY")!;
+    const anonKey = Deno.env.get("MY_SUPABASE_ANON_KEY")!;
 
     const authHeader = req.headers.get("Authorization")!;
     const callerClient = createClient(supabaseUrl, anonKey, {
